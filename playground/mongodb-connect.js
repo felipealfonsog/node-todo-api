8ee24438fb2036d50104df64c3b8cd21333cc7e5
Felipe <f.alfonso.go@gmail.com>
2017-03-22 08:12:55 -0300

    v1.1.2.0

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,6 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
@@ -6,31 +8,37 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-db.collection('Todos').insertOne({
-  text: 'Something to do',
-  completed: false
-}, (err, result) => {
-  if (err) {
-    return console.log('Unable to insert todo', err);
-  }
-  // console.log(JSON.stringify(result.aps, undefined, 2));
-  console.log(JSON.stringify(result.ops, undefined, 2));
+// db.collection('Todos').insertOne({
+//   text: 'Something to do',
+//   completed: false
+// }, (err, result) => {
+//   if (err) {
+//     return console.log('Unable to insert todo', err);
+//   }
+//   // console.log(JSON.stringify(result.aps, undefined, 2));
+//   console.log(JSON.stringify(result.ops, undefined, 2));
+//
+// });
 
-});
+
 
 //new collection . doc into Users {name, age, location}
 
-db.collection('Users').insertOne({
-  name:'Felipe',
-  age: 38,
-  location: 'Chile'
-}, (err, result) => {
-  if (err) {
-    return console.log('Unable to insert Users', err);
-  }
-  console.log(JSON.stringify(result.ops, undefined, 2));
 
-});
+
+// db.collection('Users').insertOne({
+//   name:'Felipe',
+//   age: 38,
+//   location: 'Chile'
+// }, (err, result) => {
+//   if (err) {
+//     return console.log('Unable to insert Users', err);
+//   }
+// //  console.log(JSON.stringify(result.ops, undefined, 2));
+//
+// console.log(result.ops[0]._id.getTimestamp());
+//
+// });
 
 
 
